a66ae9101754f66679bc138498918db9daf6c501
Snwolfy <97434478+Snwolfy@users.noreply.github.com>
2022-12-06 14:31:01 +0100

    refactor: adding to the branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
     const toto = async () => {
         await fetch("http://localhost:5656/", {
             method: "POST",
             mode: "cors",
         });
     };
+    return (
+        <div className="App">
+            <h1>bonjour</h1>
+        </div>
+    );
 }
 
 export default App;
