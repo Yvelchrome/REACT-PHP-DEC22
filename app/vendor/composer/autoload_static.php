2b796c10f8df41386b3f451ab18c2a4367b0cd16
Yvelchrome <yvelchrome@gmail.com>
2022-12-07 16:14:06 +0100

    feat: composer JWT dependency install

--- app/vendor/composer/autoload_static.php
+++ app/vendor/composer/autoload_static.php
@@ -7,6 +7,10 @@ namespace Composer\Autoload;
 class ComposerStaticInit58a932d732ea78827534dfbda176d8c7
 {
     public static $prefixLengthsPsr4 = array (
+        'F' => 
+        array (
+            'Firebase\\JWT\\' => 13,
+        ),
         'A' => 
         array (
             'App\\' => 4,
@@ -14,6 +18,10 @@ class ComposerStaticInit58a932d732ea78827534dfbda176d8c7
     );
 
     public static $prefixDirsPsr4 = array (
+        'Firebase\\JWT\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/firebase/php-jwt/src',
+        ),
         'App\\' => 
         array (
             0 => __DIR__ . '/../..' . '/src',
---
+++ app/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '900583c8140a3d124341c64f576b79fe1e4d7a3f',
+        'name' => 'loicjin/app',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'firebase/php-jwt' => array(
+            'pretty_version' => 'v6.3.1',
+            'version' => '6.3.1.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../firebase/php-jwt',
+            'aliases' => array(),
+            'reference' => 'ddfaddcb520488b42bca3a75e17e9dd53c3667da',
+            'dev_requirement' => false,
+        ),
+        'loicjin/app' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '900583c8140a3d124341c64f576b79fe1e4d7a3f',
+            'dev_requirement' => false,
+        ),
+    ),
+);
