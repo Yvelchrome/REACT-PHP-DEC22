b74fc10e4e772901071f820ac09a93c0512b9218
Yvelchrome <yvelchrome@gmail.com>
2022-12-07 14:37:05 +0100

    feat: header

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import React from "react";
 import "./assets/css/scss/App.scss";
 import Form from "./Form";
+import Header from "./components/Header/Header";
 
 function App() {
-  const toto = async () => {
-    await fetch("http://localhost:5656/", {
-      method: "POST",
-      mode: "cors",
-    });
-  };
   return (
     <div>
+      <Header />
       <Form />
     </div>
   );
