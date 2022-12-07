5cd02954148cf1e57bd200b9c628ac34a170ffac
Snwolfy <97434478+Snwolfy@users.noreply.github.com>
2022-12-07 14:16:33 +0100

    refactor: delete something

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,7 @@ import "./assets/css/scss/App.scss";
 import Form from "./Form";
 
 function App() {
-  const toto = async () => {
-    await fetch("http://localhost:5656/", {
-      method: "POST",
-      mode: "cors",
-    });
-  };
+
   return (
     <div>
       <Form />
